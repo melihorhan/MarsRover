5bfd5323fbba5e1d233d79bbb2640209f7dfd077
morhan <melih.orhan@yahoo.com.tr>
2020-06-25 18:12:10 +0300

    add unit test for surface

---
+++ test/MarsRover.Test/SurfaceTest.cs
@@ -0,0 +1,26 @@
+﻿using MarsRover.Application.Surface;
+using NUnit.Framework;
+
+namespace MarsRover.Test
+{
+    public class SurfaceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase(2, 2)]
+        [TestCase(3, 4)]
+        [TestCase(6, 3)]
+        [Test]
+        public void Draw__GeneratePlateauSizeWithSameValues(int expectedWidth, int expectedHeight)
+        {
+            ISurface plateau = new PlateauSurface();
+            plateau.Draw(expectedWidth, expectedHeight);
+
+            Assert.AreEqual(expectedWidth, plateau.Size.Width);
+            Assert.AreEqual(expectedHeight, plateau.Size.Height);
+        }
+    }
+}
